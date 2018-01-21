8f8078458b08ff9b832232f5a436eda43b7d13f8
Bernhard Scharrer <bernhard.scharrer@gmx.net>
2018-01-21 21:52:44 +0100

    fixed wring entries in Tier.java

--- src/bernhard/scharrer/battlemobs/util/Tier.java
+++ src/bernhard/scharrer/battlemobs/util/Tier.java
@@ -4,13 +4,13 @@ public class Tier {
 	
 	public static final int UNDEFINED = -1;
 	public static final int TIER_1_1 = 1;
-	public static final int TIER_2_1 = 2;
-	public static final int TIER_3_1 = 3;
-	public static final int TIER_1_2 = 4;
+	public static final int TIER_1_2 = 2;
+	public static final int TIER_1_3 = 3;
+	public static final int TIER_2_1 = 4;
 	public static final int TIER_2_2 = 5;
-	public static final int TIER_3_2 = 6;
-	public static final int TIER_1_3 = 7;
-	public static final int TIER_2_3 = 8;
+	public static final int TIER_2_3 = 6;
+	public static final int TIER_3_1 = 7;
+	public static final int TIER_3_2 = 8;
 	public static final int TIER_3_3 = 9;
 	
 }
