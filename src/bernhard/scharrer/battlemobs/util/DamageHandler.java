fc7118e35f9bff8e93edaed94085a849d1a325e6
Bernhard Scharrer <bernhard.scharrer@gmx.net>
2018-02-26 21:29:31 +0100

    started work on damage handler system

---
+++ src/bernhard/scharrer/battlemobs/util/DamageHandler.java
@@ -0,0 +1,26 @@
+package bernhard.scharrer.battlemobs.util;
+
+import org.bukkit.entity.Player;
+
+public class DamageHandler {
+	
+	private class PlayerDamageHandler {
+		
+		private Player p;
+		private Player last_damager;
+		
+		public PlayerDamageHandler(Player p) {
+			
+		}
+		
+		public void damage(Player p) {
+			
+		}
+		
+		public void damage() {
+			
+		}
+		
+	}
+	
+}
